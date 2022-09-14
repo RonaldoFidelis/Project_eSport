8984108304f8f44d88db493ecebba728033fc0d2
RafaelFidelis <ronaldofidelis.ti@gmail.com>
2022-09-14 00:37:12 -0300

    Status:CreateWeb

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,27 @@
-function App() {
+import './AppCss.css';
+
+
+interface H1Props {
+  value: string;
   
+};
+
+function CreateH1(props:H1Props) {
+  return (
+    <h1 className='title'>
+      {props.value}
+    </h1>
+  )
 }
 
-export default App
+function App() {
+  return (
+    <div>
+      <CreateH1 value="Texto"/>
+    </div>
+  )
+}
+
+
+
+export default App;
